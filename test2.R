eb17dc78432ea1ea1f42a67b0821ebef0d33891a
mehdizadeamir <azm0127@auburn.edu>
2021-12-02 16:21:27 -0600

    beta

--- test2.R
+++ test2.R
@@ -1,19 +1,19 @@
 library(tidyverse)
 
-df = read_csv("forestfires.csv")
+df = read_csv("crop.data.csv")
 
-X = df[,-1]
-Y = df[,1]
+X = df[,-4]
+Y = df[,4]
 # add a column with ones to the feature matrix
 shapeX = function(X) {
-        X = as.matrix(X)
-        X = cbind(1, X)
-        X
+    X = as.matrix(X)
+    X = cbind(1, X)
+    X
 }
 
 shapeY = function(Y) {
-        Y = as.matrix(Y)
-        Y
+    Y = as.matrix(Y)
+    Y
 }
 
 # Define parameters
@@ -28,19 +28,19 @@ df <- n - p
 beta0 = matrix(0, nrow  = p, ncol = 1)
 beta0[1,1] = mean(Y[,1])
 for (i in 2:p) {
-        beta0[i,1] = cov(Y[,1], X[,i]) / var(X[,i])
+    beta0[i,1] = cov(Y[,1], X[,i]) / var(X[,i])
 }
 
 L  = function(Y, X, beta){  
-        loss = sum(t(Y - X%*%beta)%*%(Y - X%*%beta))
-        loss 
+    loss = sum(t(Y - X%*%beta)%*%(Y - X%*%beta))
+    loss 
 }
 
 fit = optim(par = beta0, fn = L, X = X, Y = Y)
 beta.hat = fit$par
 
-resid <- Y - X%*%as.vector(beta.hat) 
-sigma2.hat <- (1/df)*(t(resid)%*%resid)
+resid <- Y - X%*%beta.hat
+sigma2.hat <- (1/(n -p))*t(resid)%*%resid
 
 # Estimate of the variance of the estimated beta from Eq. (6.2)
 var.beta <- as.vector(sigma2.hat)*solve(t(X)%*%X)
@@ -49,7 +49,5 @@ var.beta <- as.vector(sigma2.hat)*solve(t(X)%*%X)
 alpha = 0.5
 quant <- 1 - alpha/2
 ci.beta <- c(beta.hat - qnorm(p = quant)*sqrt(var.beta), beta.hat + qnorm(p = quant)*sqrt(var.beta))
-
-
 
 
