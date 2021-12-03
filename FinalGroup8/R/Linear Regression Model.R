b247689ef0a9df564fae390330074775407e9cb8
David Haste <dah0048@auburn.edu>
2021-12-02 23:18:52 -0600

    Creating package path and scripts for compiling

---
+++ FinalGroup8/R/Linear Regression Model.R
@@ -0,0 +1,108 @@
+#' @title Linear Regression Model
+#' 
+#' @description Contains a linear regression function that also evaluates parameters such as beta hat, confidence 
+#' intervals, and plots
+#' @param response Also known as beta, an estimation of the coefficients in the model
+#' @param predictors Also known as sigma2, an estimation of the error  
+#' @importFrom ggplot2
+#' @export
+library(ggplot2)
+
+# add a column with ones to the feature matrix
+shapeX = function(X) {
+  X = as.matrix(X)
+  X = cbind(1, X)
+  X
+}
+
+shapeY = function(Y) {
+  Y = as.matrix(Y)
+  Y
+}
+
+# least squares estimators that minimizer
+L  = function(Y, X, beta){  
+  loss = sum(t(Y - X%*%beta)%*%(Y - X%*%beta))
+  loss 
+}
+
+# define the standard quadratic loss function
+sqfn<-function(y, x, beta){  
+  loss <- sum((x%*%as.vector(beta)- y)^2)
+  return(loss) 
+}
+
+
+
+my_lm = function(response, predictors, alpha) {
+  
+  # Make sure data formats are appropriate
+  response <- shapeY(response)
+  predictors <- shapeX(predictors)
+  
+  
+  # Define parameters
+  n <- length(response)
+  p <- dim(predictors)[2]
+  df <- n - p
+  
+  # Define the initial beta
+  beta0 = matrix(0, nrow  = p, ncol = 1)
+  beta0[1,1] = mean(Y[,1])
+  for (i in 2:p) {
+    beta0[i,1] = cov(Y[,1], X[,i]) / var(X[,i])
+  }
+  
+  fit = optim(par = beta0, fn = L, predictors = predictors, response = response)
+  beta.hat = fit$par
+  
+  # Estimate of the residual variance (sigma2) from Eq. (6.3)
+  # Compute residuals
+  resids <- response - predictors%*%as.vector(beta.hat) 
+  sigma2.hat <- (1/df)*(t(resids)%*%resids)
+  
+  # Estimate of the variance of the estimated beta from Eq. (6.2)
+  var.beta <- as.vector(sigma2.hat)*solve(t(predictors)%*%predictors)
+  
+  # Estimate of the confidence interval based on alpha
+  quant <- 1 - alpha/2
+  ci.beta <- c(beta.hat - qnorm(p = quant)*sqrt(var.beta), beta.hat + qnorm(p = quant)*sqrt(var.beta))
+  
+  
+  # Get the prediction values of the responses
+  ypred<-predictors%*%as.vector(beta.hat) 
+  
+  # Calculate the R_squared values through SSE and SST
+  SSM<-sum((ypred-mean(response))^2)
+  SSE<-sum((response-ypred)^2)
+  SST<-sum((response-mean(response))^2)
+  R_squared<-1-(SSE/SST)
+  
+  # Calculate the Cp value
+  Cp<-SSE+2*P*as.numeric(sigma2.hat)
+  
+  # Calculate the F_statistics and P-value
+  DFM<-p-1
+  DFE<-n-p
+  F_statistics<-(SSM/DFM)/(SSE/DFE)
+  P_value<-pf(F_statistics,DFM,DFE)
+  
+  #Adding other measurements for the model accuracy
+  RMSE<-sqrt(sum(response-ypred)^2/n)
+  MAE<-sum(abs(response-ypred))/n
+  MAPE<-sum(abs((response-ypred)/response))/n
+  
+  #Plotting the Residual vs. fitted plots
+  r_vs_f<-ggplot() + geom_point(aes(x=resid, y= ypred))
+  
+  #Plotting the q-q plots
+  qqnorm(resids)
+  
+  #Plotting Histogram
+  plot(density(resids))
+  
+  
+  # Return all estimated values
+  return(list(beta = beta.hat, sigma2 = sigma2.hat, 
+              variance_beta = var.beta, ci = ci.beta,R-squared = R_squared, Cp = Cp, F_statistics = F_statistics, P-Value = P_value, RMSE = RMSE, MAE = MAE, MAPE = MAPE))
+}
